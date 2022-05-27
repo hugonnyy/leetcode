ee83bfbdd4e643c758098142bb8fe26c0480197a
hugonnyy <3370792085@qq.com>
2022-05-27 15:45:16 +0800

    题目27

---
+++ leetcode/27leetcode.go
@@ -0,0 +1,18 @@
+package leetcode
+
+// 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+//
+//不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+//
+//元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+func removeElement(nums []int, val int) int {
+	// 换个思路，也就是把所有不等于val的值放置到左侧
+	left := 0
+	for right := 0; right < len(nums); right++ {
+		if nums[right] != val {
+			nums[left] = nums[right]
+			left++
+		}
+	}
+	return left
+}
